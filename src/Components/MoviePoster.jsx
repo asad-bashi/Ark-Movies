06db59a33a207f837159c82444f10ce2b624f46a
Asad Bashir <asad.bashir2580@gmail.com>
2022-05-05 17:58:45 -0400

    able to load all movies from api into state for nowplaying popular toprated and upcoming movies

--- src/Components/MoviePoster.jsx
+++ src/Components/MoviePoster.jsx
@@ -1,7 +1,15 @@
-import './MoviePoster.css'
-function MoviePoster(props) {
-  const imgUrl = "https://image.tmdb.org/t/p/w500";
-  return <img className="MoviePoster" src={`${imgUrl}${props.movieImg}`} alt="" />;
-}
+import styled from "styled-components";
+const imgUrl = "https://image.tmdb.org/t/p/w500";
+const MoviePoster = styled.div`
+  width: 750px;
+  height: 100vh;
+
+  border: 5px solid red;
+  background-image: url(${(props) => `${imgUrl}${props.movieImg}`});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  cursor: pointer;
+`;
 
 export default MoviePoster;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,26 +10,43 @@ const API_KEY = "9f3a9d362ac316e4573a58e1556d4bfe";
 
 const baseUrl = "https://api.themoviedb.org/3/";
 function Home() {
-  const [movies, setMovies] = useState([]);
-
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const [popular, setPopular] = useState([]);
+  const [topRated, setTopRated] = useState([]);
+  const [upComing, setUpComing] = useState([]);
   useEffect(() => {
     async function getMovies() {
-      const { data } = await axios.get(
+      const nowPlaying = await axios.get(
         `${baseUrl}movie/now_playing?api_key=${API_KEY}`
       );
-      console.log(data);
-      setMovies(data.results);
+      setNowPlaying(nowPlaying.data.results);
+
+      const popular = await axios.get(
+        `${baseUrl}movie/popular?api_key=${API_KEY}`
+      );
+      setPopular(popular.data.results);
+
+      const topRated = await axios.get(
+        `${baseUrl}movie/top_rated?api_key=${API_KEY}`
+      );
+      setTopRated(topRated.data.results);
+
+      const upComing = await axios.get(
+        `${baseUrl}movie/upcoming?api_key=${API_KEY}`
+      );
+      setUpComing(upComing.data.results);
     }
     getMovies();
   }, []);
 
   return (
     <Box height="100vh" bgcolor={grey[900]}>
-      <Carousel className="carousel">
-        {movies.map(({ poster_path }) => (
+      <Carousel>
+        {nowPlaying.map(({ poster_path }) => (
           <MoviePoster movieImg={poster_path} />
         ))}
       </Carousel>
+      <div>hello</div>
     </Box>
   );
 }
